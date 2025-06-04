bdb865dce56204cdd900cc1950c874201cc56641
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-06-04 15:38:22 +0530

    fix build errors

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/client/header";
 import { Footer } from "@/components/shared/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Turboshop",
